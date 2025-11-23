be3621be089b79aceb10b28684ed785ab083a3c3
amitrkchauhan <62418346+amitrkchauhan@users.noreply.github.com>
2025-11-24 02:26:56 +0530

    Add files via upload

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,17 @@
+
+
+const scriptsInEvents = {
+
+	async Hunterassassin_Event92_Act6(runtime, localVars)
+	{
+
+	},
+
+	async Hunterassassin_Event101_Act7(runtime, localVars)
+	{
+		// StartLineGame();
+		window.open("https://amitrkchauhan.github.io/connectLines/", "_blank");
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
---
+++ scripts/project/typeScriptInEvents.js
@@ -0,0 +1,8 @@
+const scriptsInEvents = {
+    async Hunterassassin_Event91_Act2(runtime, localVars) {
+    },
+    async Hunterassassin_Event100_Act2(runtime, localVars) {
+    }
+};
+globalThis.C3.TypeScriptInEvents = scriptsInEvents;
+export {};
