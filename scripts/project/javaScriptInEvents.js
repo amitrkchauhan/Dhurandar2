43c8368e7155d36ed49e44d3afaeb875f5d812b5
amitrkchauhan <62418346+amitrkchauhan@users.noreply.github.com>
2025-11-26 12:31:32 +0530

    Add files via upload

--- scripts/project/javaScriptInEvents.js
+++ scripts/project/javaScriptInEvents.js
@@ -2,47 +2,17 @@
 
 const scriptsInEvents = {
 
-	async Hunterassassin_Event85_Act2(runtime, localVars)
+	async Title_Event1_Act15(runtime, localVars)
+	{
+		gtag("")
+	},
+
+	async Hunterassassin_Event90_Act6(runtime, localVars)
 	{
 
 	},
 
-	async Hunterassassin_Event86_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event87_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event88_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event89_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event90_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event91_Act1(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event101_Act6(runtime, localVars)
-	{
-
-	},
-
-	async Hunterassassin_Event112_Act7(runtime, localVars)
+	async Hunterassassin_Event145_Act6(runtime, localVars)
 	{
 		window.open("https://amitrkchauhan.github.io/connectLines/", "_blank");
 		//StartLineGame();
--- scripts/project/typeScriptInEvents.js
+++ scripts/project/typeScriptInEvents.js
@@ -1,5 +1,5 @@
 const scriptsInEvents = {
-    async Hunterassassin_Event100_Act2(runtime, localVars) {
+    async Hunterassassin_Event89_Act2(runtime, localVars) {
     }
 };
 globalThis.C3.TypeScriptInEvents = scriptsInEvents;
